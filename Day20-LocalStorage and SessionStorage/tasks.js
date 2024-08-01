00af19fb244b195f27e77502f6a59d82b74a732e
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2024-08-01 19:43:46 +0530

    Day20 Completed

--- Day20-LocalStorage and SessionStorage/tasks.js
+++ Day20-LocalStorage and SessionStorage/tasks.js
@@ -20,18 +20,31 @@ console.log(JSON.parse(localStorage.getItem("objectPerson")));
 // Activity 2: Using LocalStorage
 // • Task 3: Create a simple form that saves user input (e.g., name and email) to localstorage when submitted. Retrieve and display the saved data on page load.
 // • Task 4: Write a script to remove an item from localstorage . Log the localstorage content before and after removal.
+// For Activity 2 -> task 3 an 4 check activity2.html
 
 // Activity 3: Understanding SessionStorage
 // • Task 5: Write a script to save a string value to sessionstorage and retrieve it. Log the retrieved value.
+sessionStorage.setItem("string", "Kalpesh");
+console.log(sessionStorage.getItem("string"));
+
 // • Task 6: Write a script to save an object to sessionstorage by converting it to a JSON string. Retrieve and parse the object, then log it.
+let task6Object = {
+  language: "JS",
+  version: "1.0",
+  projectName: "JS 30Days Challenge",
+};
+sessionStorage.setItem("object", JSON.stringify(task6Object));
+console.log(JSON.parse(sessionStorage.getItem("object")));
 
 // Activity 4: Using SessionStorage
 // • Task 7: Create a simple form that saves user input (e.g., name and email) to sessionstorage when submitted. Retrieve and display the saved data on page load.
 // • Task 8: Write a script to remove an item from sessionstorage . Log the sessionstorage content before and after removal.
+// For activity 4 - task 7 & 8 check activity3.html
 
 // Activity 5: Comparing LocalStorage and SessionStorage
 // • Task 9: Write a function that accepts a key and a value, and saves the value to both localstorage and sessionstorage . Retrieve and log the values from both storage mechanisms.
 // • Task 10: Write a function that clears all data from both localstorage and sessionstorage . Verify that both storages are empty.
+// For activity 5 - task 9 & 10 check activity5.html
 
 // Feature Request:
 // 1. LocalStorage Script: Write a script that saves, retrieves, and removes items from localstorage , and displays the saved data on page load.
